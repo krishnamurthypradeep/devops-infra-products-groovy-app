ba87eaae165653743c6b13a089e56dc48f44e5dd
krishnamurthypradeep <pradeeepmurthy@gmail.com>
2023-03-14 15:18:57 +0530

    devops ci cd

---
+++ src/main/groovy/com/myapp/spring/DevopsInfraProductsGroovyAppApplication.groovy
@@ -0,0 +1,13 @@
+package com.myapp.spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DevopsInfraProductsGroovyAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DevopsInfraProductsGroovyAppApplication, args)
+	}
+
+}
